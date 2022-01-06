3f1efa2e4ad4cd23d2a45bf0bb5e263a1eaa82c8
Agustín Stringa <stringaagustin1@gmail.com>
2022-01-06 12:52:46 -0300

    First modifications

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Form } from './components/Form'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Administrador de consultas</h1>
+
+      <div className='container'>
+        Mirando el grid de skeleton
+        <div className='row'>
+          <div className='one-half column'>
+            Columna 1 de la row
+          </div>
+          <div className='one-half column'>
+            Columna 2 de la row
+          </div>
+        </div>
+        <div className='row'>
+          <div className='column'>
+            Acá iría el form
+            <Form />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <form action="" method="GET">
+      <div className="form-group">
+        <label htmlFor="">Nombre</label>
+        <input type="text" name="" id="" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="">Apellido</label>
+        <input type="text" name="" id="" />
+      </div>
+    </form>
+  );
+};
